a2ed7ab5665fb574e3f863a2d0183cc4839826ff
Kees Hiemstra <chi@xs4all.nl>
2020-01-13 11:13:31 +0100

    Add DbContextExtensions

---
+++ CHi/Extensions/DbContextExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitor.Extensions
+{
+  public static class DbContextExtensions
+  {
+
+    /// <summary>
+    /// Show the private method IsDisposed of 'System.Data.Entity.Internal.InternalContext'.
+    /// </summary>
+    /// <param name="dbContext"></param>
+    /// <returns></returns>
+    public static bool IsDisposed(this DbContext dbContext)
+    {
+
+      bool result = true;
+
+      Type typeDbContext = typeof(DbContext);
+      Type typeInternalContext =
+        typeDbContext.Assembly.GetType("System.Data.Entity.Internal.InternalContext");
+
+      FieldInfo fieldInfoInternalContext = 
+        typeDbContext.GetField("_internalContext", BindingFlags.NonPublic | BindingFlags.Instance);
+      PropertyInfo propertyInfoIsDisposed = typeInternalContext.GetProperty("IsDisposed");
+      object internalContext = fieldInfoInternalContext.GetValue(dbContext);
+
+      if (internalContext != null)
+      {
+        result = (bool)propertyInfoIsDisposed.GetValue(internalContext);
+      }
+
+      return result;
+
+    }
+
+  }
+}
