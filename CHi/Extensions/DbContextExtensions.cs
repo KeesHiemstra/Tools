79119ca562306f12e8a096da01109d128ddaea25
Kees Hiemstra <chi@xs4all.nl>
2020-01-13 11:21:52 +0100

    Made a container project to hold the generic extensions.

--- CHi/Extensions/DbContextExtensions.cs
+++ CHi/Extensions/DbContextExtensions.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeatherMonitor.Extensions
+namespace CHi.Extensions
 {
   public static class DbContextExtensions
   {
