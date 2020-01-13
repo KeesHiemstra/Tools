ee65e3ea2bf142379f10b949aa1e56414fc04155
Kees Hiemstra <chi@xs4all.nl>
2020-01-13 11:03:26 +0100

    Add PathExtensions

---
+++ CHi/Extensions/PathExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CHi.Extensions
+{
+
+  public static class PathExtensions
+  {
+    /// <summary>
+    /// Translate the PathName %OneDrive% to the full path.
+    /// %OneDrive% is a environment variable.
+    /// </summary>
+    /// <param name="PathName"></param>
+    /// <returns></returns>
+    public static string TranslatePath(this string PathName)
+    {
+      string result = PathName;
+
+      Regex regex = new Regex("%OneDrive%", RegexOptions.IgnoreCase);
+
+      if (regex.IsMatch(result))
+      {
+        result = regex.Replace(result, Environment.GetEnvironmentVariable("OneDrive"));
+      }
+
+      return result;
+    }
+  }
+
+}
