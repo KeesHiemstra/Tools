3e2a338d230023a6ec9438c6a15c207427d010b0
Kees Hiemstra <chi@xs4all.nl>
2020-04-28 09:50:30 +0200

    Updated WeatherExtensions

--- CHi/Extensions/WeatherExtensions.cs
+++ CHi/Extensions/WeatherExtensions.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CHi.Extensions
 {
 	/*
+	 * Version 1.0.0.2 (2020-02-22)
+	 * - Added: decimal ToCelsius(this double)
+	 * - Added: decimal ToKmPerHour(this double)
 	 * Version 1.0.0.1 (2020-02-01)
-	 * - Added DateTime ConvertUnixTimeToDate(int)
+	 * - Added: DateTime ConvertUnixTimeToDate(this int)
 	 */
-	 
+
 	public static class WeatherExtensions
 	{
 		#region Time
 		/// <summary>
-		/// Convert Unix time stamp (number of seconds since epoch to date/time).
+		/// Convert Unix time stamp (number of seconds since epoch to date/time.
 		/// </summary>
 		/// <param name="unixTimeStamp"></param>
 		/// <returns></returns>
-		public static DateTime ConvertUnixTimeToDate(int unixTime)
+		public static DateTime ConvertUnixTimeToDate(this int unixTime)
 		{
 			// Unix time stamp is seconds past epoch
 			DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
@@ -27,5 +26,28 @@ namespace CHi.Extensions
 		}
 		#endregion
 
+		#region Temperature
+		/// <summary>
+		/// Calculate temperature Kalvin to Celsius in 1 decimal.
+		/// </summary>
+		/// <param name="Kelvin"></param>
+		/// <returns>Celsius</returns>
+		public static decimal ToCelsius(this double Kelvin)
+		{
+			return (decimal)Math.Floor((Kelvin - 273.15) * 10) / 10;
+		}
+		#endregion
+
+		#region Wind
+		/// <summary>
+		/// Calculate the wind speed m/s to km/h.
+		/// </summary>
+		/// <param name="speed"></param>
+		/// <returns></returns>
+		public static decimal ToKmPerHour(this double speed)
+		{
+			return (decimal)(speed * 3.6);
+		}
+		#endregion
 	}
 }
