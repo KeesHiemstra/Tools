cff9fa046a88a86ba2f852a1334001f4ab82f473
Kees Hiemstra <chi@xs4all.nl>
2020-10-22 08:55:11 +0200

    WIP FallenRain

---
+++ MaintJournal/Views/FallenRainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MaintJournal.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MaintJournal.Views
+{
+    /// <summary>
+    /// Interaction logic for FallenRainWindow.xaml
+    /// </summary>
+    public partial class FallenRainWindow : Window
+    {
+        public FallenRainWindow(FallenRainViewModel viewModel)
+        {
+            InitializeComponent();
+        }
+    }
+}
