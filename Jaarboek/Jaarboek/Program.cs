761c0dd0b107eae4e781fadfa460b2ecba7b706e
Kees Hiemstra <KeesHiemstra@users.noreply.github.com>
2025-12-21 14:26:09 +0100

    Add JaarBoek

---
+++ Jaarboek/Jaarboek/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] Months = { "januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december" };
+string[] Days = { "Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag" };
+
+DateTime startDate = new DateTime(2026, 01, 01);
+DateTime endDate = new DateTime(2026, 12, 31);
+
+while (startDate <= endDate)
+{
+	Console.WriteLine($"{Days[((int)startDate.DayOfWeek)]} {startDate.Day} {Months[startDate.Month - 1]}");
+	Console.WriteLine();
+	startDate = startDate.AddDays(1);
+}
